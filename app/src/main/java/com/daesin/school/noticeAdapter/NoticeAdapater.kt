3163032713eff3d7d0d8d2f0043e7d73f27b6652
moJobSrc <leekus1005@gmail.com>
2020-09-22 01:00:05 +0900

    공지사항중 50개를 가져옴 cardview 추가 코틀린 변환 리사이클러뷰 추가 레이아웃 변경

--- app/src/main/java/com/daesin/school/noticeAdapter/NoticeAdapater.kt
+++ app/src/main/java/com/daesin/school/noticeAdapter/NoticeAdapater.kt
@@ -1,34 +1,41 @@
 package com.daesin.school.noticeAdapter
 
-import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.daesin.school.R
+import kotlinx.android.synthetic.main.notice_item.view.*
 
 class NoticeAdapater(private val noticeList: ArrayList<NoticeData>): RecyclerView.Adapter<NoticeAdapater.CustomViewHolder>() {
 
-    inner class CustomViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.notice_title)
-        val writer: TextView = itemView.findViewById(R.id.notice_writer)
-        val date: TextView = itemView.findViewById(R.id.notice_date)
-    }
+    inner class CustomViewHolder(itemView: ViewGroup): RecyclerView.ViewHolder(LayoutInflater.from(itemView.context).inflate(R.layout.notice_item, itemView, false))
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.notice_recycler, parent, false)
-        return CustomViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = CustomViewHolder(parent)
 
-    override fun getItemCount(): Int {
-        return noticeList.size
-    }
+    override fun getItemCount(): Int = noticeList.size
 
     override fun onBindViewHolder(holder: NoticeAdapater.CustomViewHolder, position: Int) {
-        holder.title.text = noticeList[position].title
-        holder.writer.text = noticeList[position].writer
-        holder.date.text = noticeList[position].date
+        with(noticeList[position]) {
+            with(holder.itemView) {
+                if (file)
+                    notice_file.visibility = View.VISIBLE
+                notice_title.text = title
+                notice_writer.text = writer
+                notice_date.text = date
+                //웹창이동
+                notice.setOnClickListener {
+                    Log.d("Notice Intent", link)
+                    it.context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("http://school.busanedu.net${link}")))
+                }
+            }
+        }
     }
 
 }
