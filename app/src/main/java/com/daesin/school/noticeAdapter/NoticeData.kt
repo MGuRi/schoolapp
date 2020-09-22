f2f518efb356b3bdec7ee2ab08c881780ce523b3
moJobSrc <leekus1005@gmail.com>
2020-09-22 10:52:50 +0900

    update

--- app/src/main/java/com/daesin/school/noticeAdapter/NoticeData.kt
+++ app/src/main/java/com/daesin/school/noticeAdapter/NoticeData.kt
@@ -1,3 +1,3 @@
 package com.daesin.school.noticeAdapter
 
-class NoticeData(val title: String, val writer: String, val date: String, val file: Boolean,val link: String)
+class NoticeData(val title: String, val writer: String, val date: String, val link: String, val file: Boolean, val important: Boolean)
